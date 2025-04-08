26e372f73ed03bfe1e13008d63db1f4e92749380
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2025-04-08 15:29:07 +0530

    nit

--- vocs/vocs.config.tsx
+++ vocs/vocs.config.tsx
@@ -32,7 +32,7 @@ export default defineConfig({
         <meta name="twitter:card" content="summary"/>
         <meta property="og:title" content="Alloy Docs" />
         <meta property="og:description" content="All things Alloy" />
-        <meta property="og:image" content="https://raw.githubusercontent.com/alloy-rs/book/master/src/images/banner.jpg" />
+        <meta property="og:image" content="https://raw.githubusercontent.com/alloy-rs/book/master/vocs/docs/publics/banner.jpg" />
       </>
     )
   },
