2d242533fb8fcedfb9985a13fc0c124c93b1551b
Matthias Seitz <matthias.seitz@outlook.de>
2025-05-15 12:14:27 +0200

    chore: bump alloy 1.0.2

--- vocs/vocs.config.tsx
+++ vocs/vocs.config.tsx
@@ -55,7 +55,7 @@ export default defineConfig({
       link: 'https://docs.rs/alloy/latest/alloy/',
     },
     { 
-      text: '1.0.1', 
+      text: '1.0.2',
       items: [ 
         { 
           text: 'Changelog', 
