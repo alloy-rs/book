eafcf123dd344daa8bf0435a147136dc105717d7
Matthias Seitz <matthias.seitz@outlook.de>
2025-05-20 17:04:31 +0200

    chore: docs bump version

--- vocs/vocs.config.tsx
+++ vocs/vocs.config.tsx
@@ -55,7 +55,7 @@ export default defineConfig({
       link: 'https://docs.rs/alloy/latest/alloy/',
     },
     { 
-      text: '1.0.3',
+      text: '1.0.5',
       items: [ 
         { 
           text: 'Changelog', 
